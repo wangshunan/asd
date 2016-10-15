235269bd6e81d28c774f55e6f98e99ad4797f12d
Chin <chinyikusougen@gmail.com>
2016-10-15 15:49:37 +0900

    123

---
+++ monsuto/Assets/Script/MSCharacterFloating.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MSCharacterFloating : MonoBehaviour {
+
+	[SerializeField] SpriteRenderer sprite;
+	[SerializeField] float range = 0.1f;
+	[SerializeField] float speed = 2f;
+	float elapsed;
+	float rand;
+
+	// Use this for initialization
+	void Start () {
+		rand = Random.value * Mathf.PI * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.unscaledDeltaTime * speed;
+		sprite.transform.localPosition = new Vector3 (0, Mathf.Sin (elapsed + rand) * range, 0);
+
+	}
+}
