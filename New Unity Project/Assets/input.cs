6bd88a11dab0387dcffff28c94d66bfc09d38611
123 <student@MacBookPro-14.local>
2017-05-12 11:58:30 +0900

    123

---
+++ New Unity Project/Assets/input.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class input : MonoBehaviour {
+
+	float x;
+	float y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		y = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		transform.position = new Vector3 (transform.position.x + x * Time.deltaTime, transform.position.y + y * Time.deltaTime, transform.position.z);
+	}
+}
